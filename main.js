6788cd1876467ef3f154b979f0cc426bbcc6790d
mounir <marnissimounir05@gmail.com>
2023-06-24 16:22:58 +0100

    first colmmit

---
+++ main.js
@@ -0,0 +1,45 @@
+const inputBox = document.getElementById("input-box");
+const listContainer = document.getElementById("list-container");
+
+//addin the task to th list
+
+function addTask() {
+  if (inputBox.value === "") {
+    alert("you must write a task");
+  } else {
+    let li = document.createElement("li");
+    li.innerHTML = inputBox.value;
+    listContainer.appendChild(li);
+    let span = document.createElement("span");
+    span.innerHTML = "\u00d7";
+    li.appendChild(span);
+  }
+  inputBox.value = "";
+  saveData();
+}
+
+//check complet tasks an ramove them
+
+listContainer.addEventListener(
+  "click",
+  function (e) {
+    if (e.target.tagName === "LI") {
+      e.target.classList.toggle("checked");
+      saveData();
+    } else if (e.target.tagName === "SPAN") {
+      e.target.parentElement.remove();
+      saveData();
+    }
+  },
+  false
+);
+
+//storethe tasks everwhen we reload the page
+
+function saveData() {
+  localStorage.setItem("data", listContainer.innerHTML);
+}
+function showTask() {
+  listContainer.innerHTML = localStorage.getItem("data");
+}
+showTask();
